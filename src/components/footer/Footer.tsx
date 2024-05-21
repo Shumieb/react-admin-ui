082c7ee49198bce36d080555017606c06c8c4665
shumieb <shumiebc@gmail.com>
2024-05-21 18:58:48 +0100

    footer styled

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -2,7 +2,10 @@ import "./footer.scss"
 
 const Footer = () =>{
     return(
-        <div className="footer">Footer</div>
+        <div className="footer">
+            <span>shumieAdmin</span>
+            <span>&copy; 2024 Admin Dashboard</span>
+        </div>
     )
 }
 
